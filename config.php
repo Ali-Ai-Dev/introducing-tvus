d887a9c379d6e6afed86810fadd49d34df280964
Ali Bozorgzad <A.bozorgzad1995@gmail.com>
2021-11-15 21:41:01 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?
+global $config;
+$config['db']['host'] = 'localhost';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['name'] = 'tvu';
+
+$config['lang'] = 'fa';
+
+$config['salt1'] = 'alwerouhv123vbrePPV84';
+$config['salt2'] = 'fkljwaeor437SDFSDqwee';
+$config['base'] = '/tvu';
+$config['route'] = array(
+  '/home' => '/page/home',
+  '/login' => '/account/login',
+  '/خانه' => '/page/home',
+  '/ورود' => '/account/login',
+);
+
